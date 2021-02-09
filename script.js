14089b673ede38d25675eb202c85193316065202
Md. Hamim Al Rashid <hamimalrashid552@gmail.com>
2021-02-09 21:30:00 +0600

    amar matha kaj kore na

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('meal-details-content');
 
 searchBtn.addEventListener('click', getMealList);
 
-function getMealList(){
+
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(res => res.json())
-    .then(data => {
-        let html ="";
-        
-    });
+        .then(res => res.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                        <div class="meal-item" data-id = "${meal.idMeal}>
+                            <div class="meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a onclick="displayMealDetails" href="#" class="recipe-btn">Get ingredient</a>
+                            </div>
+                        </div>
+                    `;
+
+                });
+            }
+
+            mealList.innerHTML = html;
+        });
 }
 
